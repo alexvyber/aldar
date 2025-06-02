1a4163846215cb2067ac96bb5dbc53f95ffd2153
Alexey Sokolov <alexey@acepicks.ai>
2025-06-02 19:41:17 +0300

    18

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,33 +1,22 @@
-type EnumValue = string | number
-type Keys<T extends Record<string, EnumValue>> = keyof T
-type Values<T extends Record<string, EnumValue>> = T[keyof T]
-type Entries<T extends Record<string, EnumValue>> = Array<
-  {
-    [Key in keyof T]: [Key, Values<Pick<T, Key>>]
-  }[keyof T]
->
+type EnumValue = string | number;
 type Prettify<T> = {
-  -readonly [K in keyof T]: T[K]
-} & {}
-export declare function Enum<const T extends Record<string, EnumValue>>(
-  obj: T,
-): Prettify<
-  T & {
+    -readonly [K in keyof T]: T[K];
+} & {};
+export declare function Enum<const T extends Record<string, EnumValue>>(obj: T): Prettify<T & {
     $: {
-      keys(): Array<keyof T>
-      values(): Array<T[keyof T]>
-      entries(): Array<
-        {
-          [Key in keyof T]: [Key, Values<Pick<T, Key>>]
-        }[keyof T]
-      >
-      mirror(): {
-        [Key in keyof T as T[Key]]: Key
-      }
-      KeysType: Keys<T>
-      ValuesType: Values<T>
-      EntriesType: Entries<T>
-    }
-  }
->
-export {}
+        keys(): Array<keyof T>;
+        values(): Array<T[keyof T]>;
+        entries(): Array<{
+            [Key in keyof T]: [Key, T[Key]];
+        }[keyof T]>;
+        mirror(): {
+            [Key in keyof T as T[Key]]: Key;
+        };
+        KeysType: keyof T;
+        ValuesType: T[keyof T];
+        EntriesType: Array<{
+            [Key in keyof T]: [Key, T[Key]];
+        }[keyof T]>;
+    };
+}>;
+export {};
--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,27 @@
 export function Enum(obj) {
-  const obj_ = { ...obj }
-  const obj__ = {}
-  Object.assign(obj__, {
-    $: {
-      values: () => values(obj_),
-      mirror: () => mirror(obj_),
-      keys: () => keys(obj_),
-      entries: () => entries(obj_),
-    },
-    ...obj_,
-  })
-  return Object.freeze(obj__)
+    const obj_ = { ...obj };
+    const obj__ = {};
+    Object.assign(obj__, {
+        $: {
+            values: () => values(obj_),
+            mirror: () => mirror(obj_),
+            keys: () => keys(obj_),
+            entries: () => entries(obj_),
+        },
+        ...obj_,
+    });
+    return Object.freeze(obj__);
 }
 function mirror(obj) {
-  return Object.freeze(
-    Object.fromEntries(Object.entries(obj).map(([left, right]) => [right, left])),
-  )
+    return Object.freeze(Object.fromEntries(Object.entries(obj).map(([left, right]) => [right, left])));
 }
 function values(obj) {
-  return Object.values(obj)
+    return Object.values(obj);
 }
 function keys(obj) {
-  return Object.keys(obj)
+    return Object.keys(obj);
 }
 function entries(obj) {
-  return Object.entries(obj)
+    return Object.entries(obj);
 }
 //# sourceMappingURL=index.js.map
